bd182acae257886338dbefff8d3e0cda33104f1b
Andreas Motl <andreas@tunfish.org>
2021-01-03 06:00:21 +0100

    Fix tests

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -16,8 +16,8 @@ def test_0815():
     assert settings.device_id == "tf-0815"
 
     assert settings.bus.broker_url == uritools.urisplit("wss://172.16.42.2:8080/ws")
-    assert settings.bus.private_key_path == configpath / "tf-0815.key"
-    assert settings.bus.certificate_path == configpath / "tf-0815.pem"
+    assert settings.bus.private_key_path == configpath / "tf-0815-bus.key"
+    assert settings.bus.certificate_path == configpath / "tf-0815-bus.pem"
 
     assert settings.wireguard.endpoint == uritools.urisplit("null://172.16.100.16:51820")
     assert settings.wireguard.private_key == "CyqisJ1eVXzjkMocWsRkAaXyXMBOxpDLFgTdDQTtXjM="
